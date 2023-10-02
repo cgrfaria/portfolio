6e628ce54614243b45dd0c5396faf7f57d5bc416
cgrfaria <cgrfaria@gmail.com>
2023-10-02 10:58:50 +0100

    NavBar done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { Montserrat } from 'next/font/google'
 import Head from 'next/head'
+import NavBar from '../components/NavBar.js'
  
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -16,8 +17,10 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
     </Head>
     <main lang="en" className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <NavBar />
       <Component {...pageProps} />
     </main>
     </>
   )
 }
+

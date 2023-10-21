6e85fbdd0c60e96a1c4f0e667f4b67ec8f187455
cgrfaria <cgrfaria@gmail.com>
2023-10-21 14:49:59 +0100

    changing viewport width of mobile menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -131,7 +131,7 @@ const NavBar = () => {
         <motion.div
           initial={{scale:0, opacity:0, x: "-50%", y: "-50%"}}
           animate={{scale:1, opacity:1}} 
-          className='min-w-[70vw] flex flex-col justify-between items-center bg-dark/90
+          className='min-w-[90vw] flex flex-col justify-between items-center bg-dark/90
           fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 rounded-lg backdrop-blur-md py-32'>
 
           <nav className='flex flex-col items-center justify-center'>
